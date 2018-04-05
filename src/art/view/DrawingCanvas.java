d5ff39dd8b4c572df42aa2757ee1cb343386815f
greenRose2018 <brifun13@gmail.com>
2018-04-04 22:11:43 -0600

    added imports to DrawingCanvas

--- src/art/view/DrawingCanvas.java
+++ src/art/view/DrawingCanvas.java
@@ -1,6 +1,17 @@
 package art.view;
 
 import javax.swing.JPanel;
+import javax.swing.JFileChooser;
+import javax.imageio.ImageIO;
+
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import art.controller.ArtController;
 
 public class DrawingCanvas extends JPanel
 {
