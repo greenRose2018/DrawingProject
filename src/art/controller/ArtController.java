949b5525c38ce80956fbac87289a3f01bf8013e8
greenRose2018 <brifun13@gmail.com>
2018-04-04 23:02:31 -0600

    added imports to Controller
    created new methods getFrame() and handleErrors().

--- src/art/controller/ArtController.java
+++ src/art/controller/ArtController.java
@@ -1,15 +1,32 @@
 package art.controller;
 
+import java.awt.*;
+import javax.swing.*;
+import java.io.IOException;
+import art.view.*;
+
 public class ArtController
 {
+	private ArtFrame artFrame;
 
 	public ArtController()
 	{
-		
+		artFrame = new ArtFrame();
 	}
 	
 	public void Start()
 	{
 		
+	}
+	
+
+	public JFrame getFrame() 
+	{
+		return artFrame;
+	}
+
+	public void handleErrors(IOException error) 
+	{
+		System.out.println(error.getMessage());
 	}
 }
